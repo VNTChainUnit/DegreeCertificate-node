3a28d7e677e5455f7124b982a47c7ac23585fa38
lemon <601749122@qq.com>
2021-08-04 22:50:23 +0800

    change package name and format code

--- src/app/service/provider.ts
+++ src/app/service/provider.ts
@@ -2,44 +2,44 @@ import { DubboProvider, dubbo } from '@dazejs/dubbo-provider';
 import BlocckChainService from './blockchain/main';
 
 @dubbo.registry('default')
-@dubbo.interface('com.xinyi.dc.service.BlockChainService')
+@dubbo.interface('com.xinyi.dc.blockchain.api.service.BlockChain')
 export default class extends DubboProvider {
-  
+
   @dubbo.method()
   hello(name: string) {
     return `Hello ${name}`;
   }
 
   @dubbo.method()
-  async getCertificate(certificateNumber:string, idnumber:string){
-    var res:string = await BlocckChainService.getCertificate(certificateNumber,idnumber);
+  async getCertificate(certificateNumber: string, idnumber: string) {
+    var res: string = await BlocckChainService.getCertificate(certificateNumber, idnumber);
     return res;
   }
 
   @dubbo.method()
-  async addCertificate(school:string, name:string, idnumber:string, degreeType:string, major:string, graduationDate:string, studentNumber:string, certificateNumber:string){
-    var res = await BlocckChainService.addCertificate(school,name,idnumber,degreeType,major,graduationDate,studentNumber,certificateNumber);
-    if(!res)res = "";
+  async addCertificate(school: string, name: string, idnumber: string, degreeType: string, major: string, graduationDate: string, studentNumber: string, certificateNumber: string) {
+    var res = await BlocckChainService.addCertificate(school, name, idnumber, degreeType, major, graduationDate, studentNumber, certificateNumber);
+    if (!res) res = "";
     return res;
   }
 
-@dubbo.method()
-async existCertificate(name:string, studentNumber:string, school:string, idnumber:string){
-  var res = await BlocckChainService.existCertificate(name,studentNumber,school,idnumber);
-  return res;
-}
+  @dubbo.method()
+  async existCertificate(name: string, studentNumber: string, school: string, idnumber: string) {
+    var res = await BlocckChainService.existCertificate(name, studentNumber, school, idnumber);
+    return res;
+  }
 
-@dubbo.method()
-async checkCertificate(certificateNumber:string, name:string, school:string, degreeType:string, graduationDate:string, major:string){
-  var res = await BlocckChainService.checkCertificate(certificateNumber,name,school,degreeType,graduationDate,major);
-  return res;
-}
+  @dubbo.method()
+  async checkCertificate(certificateNumber: string, name: string, school: string, degreeType: string, graduationDate: string, major: string) {
+    var res = await BlocckChainService.checkCertificate(certificateNumber, name, school, degreeType, graduationDate, major);
+    return res;
+  }
 
-@dubbo.method()
-async getTransactionNumber(transactionHash:string){
-  var tx = await BlocckChainService.getTransaction(transactionHash);
-  return tx['blockNumber'];
-}
+  @dubbo.method()
+  async getTransactionNumber(transactionHash: string) {
+    var tx = await BlocckChainService.getTransaction(transactionHash);
+    return tx['blockNumber'];
+  }
 
 
 }
--- src/app/service/utils.ts
+++ src/app/service/utils.ts
@@ -3,21 +3,21 @@ import crypto from 'crypto';
 
 const secret = Private.secret;
 
-function aesEncrypt(data:string) {
+function aesEncrypt(data: string) {
     const cipher = crypto.createCipher('aes192', secret);
     var crypted = cipher.update(data, 'utf8', 'hex');
     crypted += cipher.final('hex');
     return crypted;
 }
- 
-function aesDecrypt(encrypted:string) {
+
+function aesDecrypt(encrypted: string) {
     const decipher = crypto.createDecipher('aes192', secret);
     var decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
 }
 
-export  default{
-    aesEncrypt:aesEncrypt,
-    aesDecrypt:aesDecrypt
+export default {
+    aesEncrypt: aesEncrypt,
+    aesDecrypt: aesDecrypt
 }
