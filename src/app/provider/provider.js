b1cf046876a97c73912be8bd2cd8c98eac1b9051
lemon <601749122@qq.com>
2021-08-09 14:03:48 +0800

    decrypt certificate

--- src/app/provider/provider.js
+++ src/app/provider/provider.js
@@ -1,6 +1,7 @@
 import { DubboProvider, dubbo } from '@dazejs/dubbo-provider';
 import BlocckChainService from '../service/blockchain/main';
 import dubboConfig from '../../config/dubbo'
+import utils from '../service/utils'
 @dubbo.registry('default')
 @dubbo.interface('com.xinyi.dc.blockchain.node.BlockChain')
 @dubbo.ip(dubboConfig.default.ip)
@@ -13,7 +14,20 @@ export default class extends DubboProvider {
 
   @dubbo.method()
   async getCertificate(certificateNumber  , idnumber  ) {
-     var res  = await BlocckChainService.getCertificate(certificateNumber, idnumber);
+     var certificateres  = await BlocckChainService.getCertificate(certificateNumber, idnumber);
+     let dataarr=certificateres.split("|")
+     //对每个字段解密
+     let certificate={
+     "school":utils.aesDecrypt(dataarr[0]),
+     "name":utils.aesDecrypt(dataarr[1]),
+     "idnumber":utils.aesDecrypt(dataarr[2]),
+     "degreeType":utils.aesDecrypt(dataarr[3]),
+     "major":utils.aesDecrypt(dataarr[4]),
+     "graduationDate":utils.aesDecrypt(dataarr[5]),
+     "studentNumber":utils.aesDecrypt(dataarr[6]),
+     "certificateNumber":utils.aesDecrypt(dataarr[7])
+     }
+     let res = JSON.stringify(certificate);
     return res;
   }
 
